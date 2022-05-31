960e0bdc2d57c265b00cfc285d341f3a5dddddaa
wendyvosu <102492464+wendyvosu@users.noreply.github.com>
2022-05-31 13:51:07 -0400

    index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+const { escapeRegExpChars } = require("ejs/lib/utils")
 const express = require("express")
 const app = express()
 const port = 3000
+const pokemon = require("./models/pokemon")
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Pokemon App!");
+})
+
+app.get("/pokemon", (req, res) => {
+    res.render("index", {data:pokemon});
 })
 
 app.listen(port, () => {
